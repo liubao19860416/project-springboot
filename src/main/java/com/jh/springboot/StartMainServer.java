107a2b1e0ba82a3e1f5f8bea84e35b295fa8bb97
18611478781 <18611478781@163.com>
2016-05-09 11:20:28 +0800

    xiugai

--- src/main/java/com/jh/springboot/StartMainServer.java
+++ src/main/java/com/jh/springboot/StartMainServer.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
@@ -16,33 +17,28 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
  * @2016年5月8日
  *
  */
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
 //@ImportResource("classpath*:spring-context.xml")
 //@EnableScheduling
 //@SpringBootApplication(exclude = {GithubRepo.class})
 
 @SpringBootApplication
 @EnableAdminServer
-//@EnableAutoConfiguration
-//@ComponentScan("com.jh.springboot.*")
+@ComponentScan("com.jh.springboot")
 public class StartMainServer {
     
     private static final Logger logger = LoggerFactory.getLogger(StartMainServer.class);
     
     public static void main(String[] args) {
         logger.info("启动项目配置开始...");
-//        SpringApplication.run(StartMainServer.class, args);
+        SpringApplication.run(StartMainServer.class, args);
         
-        SpringApplication app = new SpringApplication(StartMainServer.class);  
-        app.setWebEnvironment(true);  
-        app.setShowBanner(false);  
-          
-        Set<Object> set = new HashSet<Object>();  
-        //set.add("classpath:applicationContext.xml");  
-        app.setSources(set);  
-        app.run(args);  
+//        SpringApplication app = new SpringApplication(StartMainServer.class);  
+//        app.setWebEnvironment(true);  
+//        app.setShowBanner(false);  
+//        Set<Object> set = new HashSet<Object>();  
+//        //set.add("classpath:applicationContext.xml");  
+//        app.setSources(set);  
+//        app.run(args);  
         logger.info("启动项目配置结束...");
     }  
     
